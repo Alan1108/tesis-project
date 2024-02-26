fb6d81f6d803549171cd638c89aae9115ecf1c82
Alan1108 <alan.ibm1108@gmail.com>
2024-02-25 21:38:15 -0500

    delete: carpeta front reubicada bajo la carpeta ui

--- front/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import router from './router'; // Importa el archivo de configuración del router
-
-createApp(App).use(router).mount('#app');
