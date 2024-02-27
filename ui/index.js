8d44e7332fe51b72beca1e37a5d3291c19ebd1f1
alan-bermudez-wp <ext.abermudez@warbyparker.com>
2024-02-26 21:14:57 -0500

    fix: se agregaron las rutas del frontend al servicio de express para el despliegue

--- ui/index.js
+++ ui/index.js
@@ -1,13 +1,19 @@
 import exppress from 'express';
+import path from 'path'
 const app = exppress();
+const __dirname = path.resolve();
 app.use(exppress.json());
-app.use(exppress.static('front/dist'));
+app.use(exppress.static(path.join(__dirname, 'front/dist')));
 const port = process.env.PORT || 8080;
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 });
 
-app.get('/health_check',(req, res) => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front/dist/index.html'));
+  });
+
+app.get('/',(req, res) => {
     res.status(200).send({ msg: "Frontend is working properly"});
 });
